ca851dac9617aa9e7a2f43ea842e250b7da8c50d
PLeeAnh <anhpltp00872@gmail.com>
2025-07-04 14:23:23 +0700

    init project
    Tôi khởi tạo dự án

--- GameOnline/src/view/LoginJFrame.java
+++ GameOnline/src/view/LoginJFrame.java
@@ -152,6 +152,12 @@ public class LoginJFrame extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
--- GameOnline/src/view/MainJFrame.java
+++ GameOnline/src/view/MainJFrame.java
@@ -10,12 +10,12 @@ import javax.swing.JPanel;
  *
  * @author thanh
  */
-public class HomeJFrame extends javax.swing.JFrame {
+public class MainJFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form HomeJFrame
      */
-    public HomeJFrame() {
+    public MainJFrame() {
         initComponents();
         setLocationRelativeTo(null);
         showPanel(new AnhBiaJPanel());
@@ -191,21 +191,23 @@ public class HomeJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(HomeJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(HomeJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(HomeJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(HomeJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new HomeJFrame().setVisible(true);
+                new MainJFrame().setVisible(true);
             }
         });
     }
